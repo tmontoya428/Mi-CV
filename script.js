8dfe59987647cdfa26f29200a2a62772a3939332
Juan Granja <4684593@gmail.com>
2026-07-08 17:06:55 -0500

    se sube modificaciones del emailjs

--- script.js
+++ script.js
@@ -76,18 +76,27 @@
         });
     }, {offset: '80%'});
 
-
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
+ //   if ($('.portfolio-container').length) {
+
+  //  var portfolioIsotope = $('.portfolio-container').isotope({
+    //    itemSelector: '.portfolio-item',
+      //  layoutMode: 'fitRows'
+  //  });
+
+  //  $('#portfolio-flters li').on('click', function () {
+
+    //    $("#portfolio-flters li").removeClass('active');
+    //    $(this).addClass('active');
+
+      //  portfolioIsotope.isotope({
+        //    filter: $(this).data('filter')
+       // });
+
+  //  });
+
+//}
     
     
     
